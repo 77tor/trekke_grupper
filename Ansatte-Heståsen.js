6c0a386f2616fe12cd2515a74f0cc68e6c6891bc
77tor <77tor@ikrs.no>
2026-08-26 11:12:53 +0200

    Add files via upload

---
+++ Ansatte-Heståsen.js
@@ -0,0 +1,19 @@
+// Ansatte-Heståsen.js
+window.classLists = window.classLists || {};
+
+window.classLists["Ansatte Heståsen"] = [
+  "Daniel",
+  "Elise",
+  "Christine",
+  "Truls",
+  "Geir",
+  "Åse Reidun",
+  "Markus",
+  "Eddy",
+  "Ingrid",
+  "Anders",
+  "Tonje",
+  "Hanne",
+  "Ingrid Hanne",
+  "Filip"
+];
