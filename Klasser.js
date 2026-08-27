3f2fd210c6619224acb4ab89a32cd32c533a01f6
77tor <77tor@ikrs.no>
2026-08-27 11:10:11 +0200

    Add files via upload

---
+++ Klasser.js
@@ -0,0 +1,88 @@
+// Klasser.js
+window.classLists = window.classLists || {};
+
+// 1. TRINN
+window.classLists["Klasse 1A"] = [
+  "Ngus",
+  "Storm Alfred",
+  "Linnéa",
+  "Thea",
+  "Jeremiah",
+  "Bendik",
+  "Levi",
+  "Tilie",
+  "Anna",
+  "Astrid",
+  "Gard",
+  "Rikke",
+  "John Edvard",
+  "Amina",
+  "Matheo",
+  "Ayla Sofie",
+  "Karen",
+  "Liana",
+  "Embla Tomine",
+  "Alma",
+  "Amandus",
+  "Johannes",
+  "Trygve",
+  "Jennie"
+];
+
+window.classLists["Klasse 1B"] = [
+  "Amina",
+  "Amalie",
+  "Zayaan",
+  "Konrad",
+  "Dan Gebriel",
+  "Amalie",
+  "Erian",
+  "Kasper",
+  "Elias",
+  "Miriam",
+  "Noomi Olava",
+  "David",
+  "Odin",
+  "Ulrik",
+  "Lana",
+  "Nora",
+  "Ylva",
+  "Sofia",
+  "Alma",
+  "Silje",
+  "Alena",
+  "Lily",
+  "Milian",
+  "Andrea"
+];
+
+// 2. TRINN
+window.classLists["Klasse 2A"] = [];
+window.classLists["Klasse 2B"] = [];
+window.classLists["Klasse 2C"] = [];
+
+// 3. TRINN
+window.classLists["Klasse 3A"] = [];
+window.classLists["Klasse 3B"] = [];
+window.classLists["Klasse 3C"] = [];
+
+// 4. TRINN
+window.classLists["Klasse 4A"] = [];
+window.classLists["Klasse 4B"] = [];
+window.classLists["Klasse 4C"] = [];
+
+// 5. TRINN
+window.classLists["Klasse 5A"] = [];
+window.classLists["Klasse 5B"] = [];
+window.classLists["Klasse 5C"] = [];
+
+// 6. TRINN
+window.classLists["Klasse 6A"] = [];
+window.classLists["Klasse 6B"] = [];
+window.classLists["Klasse 6C"] = [];
+window.classLists["Klasse 6D"] = [];
+
+// 7. TRINN
+window.classLists["Klasse 7A"] = [];
+window.classLists["Klasse 7B"] = [];
+window.classLists["Klasse 7C"] = [];
